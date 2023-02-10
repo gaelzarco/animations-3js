e772835f3541ff359ca68f45bd5bef5202e03fc5
gael <76548719+gaelzarco@users.noreply.github.com>
2023-02-10 13:49:25 -0800

    initial tick running

--- src/script.js
+++ src/script.js
@@ -29,3 +29,11 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+// Animations
+const tick = () => {
+    console.log('tick')
+    window.requestAnimationFrame(tick)
+}
+
+tick()
